3fe6554ecaf795f0906c44ca3634829671d70a65
Hidetaka Okamoto <hideokamoto@stripe.com>
2025-02-21 10:29:36 +0900

    ai-chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,17 +1,19 @@
 // import { getRequestContext } from '@cloudflare/next-on-pages'
 import { getRequestContext } from "@cloudflare/next-on-pages";
 import { createWorkersAI } from 'workers-ai-provider';
-import { streamText } from 'ai';
+import { CoreMessage, streamText } from 'ai';
 
 
 export const runtime = 'edge'
 
-export async function GET() {
+export async function POST(req: Request) {
+  const { messages } = await req.json<{messages: CoreMessage[]}>();
+  console.log(messages)
   const ai = getRequestContext().env.AI
   const workersai = createWorkersAI({ binding: ai });
   const result = streamText({
-    model: workersai('@cf/meta/llama-2-7b-chat-int8'),
-    prompt: 'Write a 50-word essay about hello world.',
+    model: workersai('@cf/deepseek-ai/deepseek-r1-distill-qwen-32b'),
+    messages,
   })
   return result.toDataStreamResponse();
 }
